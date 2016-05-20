dcb64cc428d7add81167f1839572851ebf8d60f6
zdoonio <dominik.zdunczyk@gmail.com>
2016-05-21 01:32:44 +0200

    statystyki gracza

--- Assets/_scripts/animationAttack.cs
+++ Assets/_scripts/animationAttack.cs
@@ -3,15 +3,21 @@ using System.Collections;
 
 public class animationAttack : MonoBehaviour {
 	public Animation attackAnim;
+	public AudioClip oneSlice;
+
+	private AudioSource cutAudio;
 	// Use this for initialization
 	void Start () {
 		attackAnim = GetComponent<Animation>();
+		cutAudio = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetButtonDown ("Fire1")) {
 			attackAnim.Play ("slice", PlayMode.StopAll);
+			cutAudio.PlayOneShot (oneSlice, 1F);
+
 		}
 	}
 }
---
+++ Assets/_scripts/playerStats.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class playerStats : MonoBehaviour {
+
+	private float maxHealth = 100;
+	private float currentHealth = 100;
+	private float maxStamina = 100;
+	private float currentStamina = 100;
+	public Text healthText;
+	public Text staminaText;
+
+	private float canHeal = 0.0f;
+	private float canRegenerate = 0.0f;
+
+	private CharacterController chCont;
+	private UnityStandardAssets.Characters.FirstPerson.FirstPersonController fpsC;
+	private Vector3 lastPosition;
+
+	void Awake()
+	{
+		chCont = GetComponent<CharacterController>();
+		fpsC = gameObject.GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController> ();
+		lastPosition = transform.position;
+	}
+
+
+	void Start()
+	{
+
+	}
+
+	void Update()
+	{
+		healthText.text =  "Health: " +  currentHealth;
+		staminaText.text = "Stamina: " +  currentStamina;
+
+		if ((Input.GetButtonDown ("Fire1"))){
+			currentStamina -= 30;
+
+			}
+			
+
+		if(canHeal > 0.0f) {
+			canHeal -= Time.deltaTime;
+		}
+		if(canRegenerate > 0.0f) {
+			canRegenerate -= Time.deltaTime;
+		}
+
+		if(canHeal <= 0.0f && currentHealth < maxHealth) {
+			regenerate(ref currentHealth, maxHealth);
+		}
+		if(canRegenerate <= 0.0f && currentStamina < maxStamina) {
+			regenerate(ref currentStamina, maxStamina);
+		}
+
+	}
+
+	void FixedUpdate () 
+	{
+		if(chCont.isGrounded && Input.GetKey(KeyCode.LeftShift)) {
+			lastPosition = transform.position;
+			currentStamina -= 1;
+			currentStamina = Mathf.Clamp(currentStamina, 0, maxStamina);
+		}	
+
+		if (currentStamina > 0) {
+			fpsC.m_RunSpeed = 7;
+		} else {
+			fpsC.m_RunSpeed = 3;
+		}	
+	}
+
+	void takeHit(float damage) 
+	{
+		currentHealth -= damage;
+
+		if(currentHealth < maxHealth) {
+			canHeal = 5.0f;
+		}
+
+		currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+	}
+
+	void regenerate(ref float currentStat, float maxStat)
+	{
+		currentStat += maxStat * 0.005f;
+		Mathf.Clamp(currentStat, 0, maxStat);
+	}
+
+}
