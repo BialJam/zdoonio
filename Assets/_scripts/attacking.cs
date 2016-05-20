5705da3561a641c71ea8a1c0337993d5b3a3a9c5
zdoonio <dominik.zdunczyk@gmail.com>
2016-05-20 23:19:40 +0200

    Initial

---
+++ Assets/_scripts/attacking.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class attacking : MonoBehaviour {
+
+	//public Animation attackAnim;
+	public float hitDelay = 0.5f;
+	//public Vector3 promien;
+
+
+	private float hitDelayCounter = 0.0f;
+	private Vector3 fwd;
+	private RaycastHit hit;
+	private float range = 3.0f;
+
+	// Use this for initialization
+	void Start () {
+
+		//attackAnim = GetComponent<Animation>();
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+	 
+		fwd = transform.TransformDirection(Vector3.forward);
+
+
+
+
+		if (Input.GetButtonDown ("Fire1")) {
+				hitDelayCounter = hitDelay;
+				
+				if (Physics.Raycast (transform.position, fwd, out hit)) {
+					if (hit.transform.tag == "Enemy" && hit.distance < range) {
+						Debug.Log ("Trafiony przeciwnik");
+
+					} else if (hit.distance < range) {
+						Debug.Log ("Trafiona Sciana");
+					}
+				}
+			}			
+		}
+	}
